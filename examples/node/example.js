203ad5e4c7ae99e28b9af4ece3ddab5aea62d955
Umakant Patil <umakantp@glam.com>
2013-12-18 22:25:18 -0800

    Added example for node.js and npm package.json file.

---
+++ examples/node/example.js
@@ -0,0 +1,14 @@
+/**
+ * Example show jSmart can be used in Node.js
+ */
+
+var fs = require('fs'),
+    path = require('path');
+
+require(path.normalize(__dirname+'/../../jsmart.js'));
+
+var tpl = fs.readFileSync(path.normalize(__dirname+'/hello.tpl'), {encoding: 'utf-8'});
+
+var compiledTemplate = new jSmart(tpl);
+
+console.log(compiledTemplate.fetch({name: "Umakant Patil"}));
---
+++ examples/node/hello.tpl
@@ -0,0 +1,5 @@
+Hello People..
+
+This template is rendered from Node.js
+
+By {$name}
