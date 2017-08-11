2c9cf645c02b755d8e4eb34847fd1afec5efd3be
Umakant Patil <umakant.patil@globant.com>
2017-08-11 14:34:52 +0530

    Do no overwrite object, merge it to maintain reference

--- jsmart.js
+++ jsmart.js
@@ -1814,6 +1814,24 @@
         }
         return res;
     }
+	
+	function MergeRecursive(obj1, obj2) {
+        for (var p in obj2) {
+            try {
+                // Property in destination object set; update its value.
+                if ( obj2[p].constructor==Object ) {
+                    obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+
+                } else {
+                    obj1[p] = obj2[p];
+                }
+            } catch(e) {
+                // Property in destination object not set; create it and set its value.
+                obj1[p] = obj2[p];
+            }
+        }
+        return obj1;
+	}
 
     function getTemplate(name, tree, nocache)
     {
@@ -1829,7 +1847,8 @@
         }
         else
         {
-            tree = files[name];
+            MergeRecursive(tree, files[name]);
+            //tree = files[name];
         }
         return tree;
     }
